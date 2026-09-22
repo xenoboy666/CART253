9c70b163849240821c77409c843cfc319f3e9c35
xenoboy666 <nathan.vallee2002@gmail.com>
2026-09-22 17:55:16 -0400

    created painting 1 and 2

--- Prototypes/instructions-prototype-1/template-p5-project (1)/template-p5-project/js/script.js
+++ Prototypes/instructions-prototype-1/template-p5-project (1)/template-p5-project/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-createCanvas(500, 500);
+createCanvas(500,500);
 }
 
 
@@ -21,15 +21,128 @@ createCanvas(500, 500);
 */
 function draw() {
 //background color
-background(220, 2, 250);
-}
-//call the function-run the code
-drawRectangle();
+background(220,2,250);
 
-//draw rectangle
-function drawRectangle() {
+
+//call the function-run the code
+drawGreeny();
+drawRedy();
+drawBluey();
+drawYellowy();
+drawOrangey();
+drawPurpley();
+drawLightGreeny();
+drawPinky();
+drawWhitey();
+drawBlacky();
+drawLightBluey();
+drawBrownTriangle();
+drawGreyOval();
+drawDarkBlueOval();
+drawLightBlueOval();
+}
+//draw green rectangle
+function drawGreeny() {
+    push();
+    fill(0, 152, 25);
+    rect(100, 100, 200, 200);
+    pop();
+}
+//draw red rectangle
+function drawRedy() {
+    push();
+    fill(255, 0, 0);
+    rect(150, 150, 100, 100);
+    pop();
+}
+//draw blue rectangle
+function drawBluey() {
     push();
     fill(0, 0, 255);
-    rect(100, 100, 200, 200);
+    rect(175, 175, 50, 50);
+    pop();
+}
+//draw yellow rectangle
+function drawYellowy() {
+    push();
+    fill(255, 255, 0);
+    rect(450, 1, 25, 500);
+    pop();
+}
+//draw orange rectangle
+function drawOrangey() {
+    push();
+    fill(255, 165, 0);
+    rect(400, 1, 25, 500);
+    pop();
+}
+//draw purple arc 
+function drawPurpley() {
+    push();
+    fill(128, 0, 128);
+    arc(100, 500, 375, 375, PI, TWO_PI);
+    pop();
+}
+//draw light green arc
+function drawLightGreeny() {
+    push();
+    fill(144, 238, 144);
+    arc(100, 500, 300, 300, PI, TWO_PI);
+    pop();
+}
+//draw pink arc
+function drawPinky() {
+    push();
+    fill(255, 192, 203);
+    arc(100, 500, 225, 225, PI, TWO_PI);
+    pop();
+}
+//draw white arc
+function drawWhitey() {
+    push();
+    fill(255, 255, 255);
+    arc(100, 500, 150, 150, PI, TWO_PI);
+    pop();
+}
+//draw black arc
+function drawBlacky() {
+    push();
+    fill(0, 0, 0);
+    arc(100, 500, 75, 75, PI, TWO_PI);
+    pop();
+}
+//draw light blue triangle
+function drawLightBluey() {
+    push();
+    fill(173, 216, 230);
+    triangle(150, 170, 200, 1, 100, 1);
+    pop();
+}
+//draw brown triangle
+function drawBrownTriangle() {
+    push();
+    fill(165, 42, 42);
+    triangle(245, 270, 270, 1, 220, 1);
+    pop();
+}
+//draw grey oval
+function drawGreyOval() {
+    push();
+    fill(128, 128, 128);
+    ellipse(400, 200, 200, 50);
+    pop();
+}
+//draw dark blue oval   
+function drawDarkBlueOval() {
+    push();
+    fill(0, 0, 139);
+    ellipse(400, 200, 100, 25);
+    pop();
+}
+//draw light blue oval
+function drawLightBlueOval() {
+    push();
+    fill(173, 216, 230);
+    ellipse(400, 200, 50, 12.5);
     pop();
 }
--- Prototypes/instructions-prototype-2/template-p5-project (1)/template-p5-project/js/script.js
+++ Prototypes/instructions-prototype-2/template-p5-project (1)/template-p5-project/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+createCanvas(500,500);
 }
 
 
@@ -20,5 +20,135 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+//background color
+background(1,2,1);
+//call the function-run the code<
+drawAlienBody();
+drawAlienHead();
+drawEyeArm1();
+drawEyeArm2();
+drawEye1();
+drawEye2();
+drawIris1();
+drawIris2();
+drawPupil1();
+drawPupil2();
+drawAlienMouth();
+drawAlienTongue();
+drawAlienTeeth1();
+drawAlienTeeth2();
+drawHat();
+drawPonpon();
+}
+//draw alien body
+function drawAlienBody() {
+    push();
+    fill(0, 255, 0);
+    rect(250, 300, 50, 200);
+    pop();
+}
+//draw alien head
+function drawAlienHead() {
+    push();
+    fill(0, 255, 0);
+    ellipse(275, 220, 200, 200);
+    pop();
+}
+//draw eye arm 1 
+function drawEyeArm1() {
+    push();
+    fill(0, 255, 0);
+    quad(175, 220, 75, 100, 85, 100, 175, 200);
+    pop();
+}
+//draw eye arm 2
+function drawEyeArm2() {
+    push();
+    fill(0, 255, 0);
+    quad(375, 220, 425, 400, 435, 400, 375, 200);
+    pop();
+}
+//draw eye 1
+function drawEye1() {
+    push();
+    fill(255, 255, 255);
+    ellipse(100, 100, 85, 85);
+    pop();
+}
+//draw eye 2
+function drawEye2() {
+    push();
+    fill(255, 255, 255);
+    ellipse(425, 400, 85, 85);
+    pop();
+}
+//draw iris 1
+function drawIris1() {
+    push();
+    fill(0, 0, 255);
+    ellipse(100, 100, 50, 50);
+    pop();
+}
+//draw iris 2
+function drawIris2() {
+    push();
+    fill(0, 0, 255);
+    ellipse(425, 400, 50, 50);
+    pop();
+}
+//draw pupils 1
+function drawPupil1() {
+    push();
+    fill(0, 0, 0);
+    ellipse(100, 100, 25, 25);
+    pop();
+}
+//draw pupils 2 
+function drawPupil2() {
+    push();
+    fill(0, 0, 0);
+    ellipse(425, 400, 25, 25);
+    pop();
+}
+//draw alien mouth
+function drawAlienMouth() {
+    push();
+    fill(255, 0, 0);
+    arc(275, 240, 100, 100, 200, PI);
+    pop();
+}
+//draw alien tongue
+function drawAlienTongue() {
+    push();
+    fill(255, 170, 170);
+    ellipse(285, 275, 50, 30);
+    pop();
+}
+//draw alien teeth 1
+function drawAlienTeeth1() {
+    push();
+    fill(255, 255, 255);
+    triangle(225, 240, 250, 240, 237, 270);
+    pop();
+}
+//draw alien teeth 2
+function drawAlienTeeth2() {
+    push();
+    fill(255, 255, 255);
+    triangle(295, 200, 315, 230, 280, 230);
+    pop();
+}
+//draw hat
+function drawHat() {
+    push();
+    fill(105, 255, 200);
+    triangle(240, 125, 300, 125, 275, 50);
+    pop();
+}
+//draw ponpon
+function drawPonpon() {
+    push();
+    fill(255, 0, 255);
+    ellipse(275, 50, 25, 25);
+    pop();
 }
