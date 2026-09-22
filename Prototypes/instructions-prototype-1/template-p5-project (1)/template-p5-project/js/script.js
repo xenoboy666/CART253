f86849b08da0a91b9ea418c4ae3903407bf0ba18
xenoboy666 <nathan.vallee2002@gmail.com>
2026-09-22 16:28:34 -0400

    Update script.js

--- Prototypes/instructions-prototype-1/template-p5-project (1)/template-p5-project/js/script.js
+++ Prototypes/instructions-prototype-1/template-p5-project (1)/template-p5-project/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+createCanvas(500, 500);
 }
 
 
@@ -20,5 +20,16 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+//background color
+background(220, 2, 250);
+}
+//call the function-run the code
+drawRectangle();
 
+//draw rectangle
+function drawRectangle() {
+    push();
+    fill(0, 0, 255);
+    rect(100, 100, 200, 200);
+    pop();
 }
