13ff933abb47174781f448bda566727f109901ce
xenoboy666 <nathan.vallee2002@gmail.com>
2026-09-23 19:07:56 -0400

    exercise class week 3

--- Prototypes/Week 3/Variables/exercise/js/script.js
+++ Prototypes/Week 3/Variables/exercise/js/script.js
@@ -11,16 +11,51 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
+
+let ellipse_Size = 550;
+let rect_Size=10;
+let sunX=20;
+let sunY=20;
+let sunSize=10;
+let r=255;
+let g=255;
+let b=0;
+
+let sun = {
+    sunX:20,
+    sunY:50,
+    sunSize:50,
+    r:255,
+    g:255,
+    b:0
+}
+
+
+let bird = {
+    birdX:200,
+    birdY:50,
+    birdSize:20,
+    r:0,
+    g:0,
+    b:255
+}
 function setup() {
-createCanvas(700,700);
+createCanvas(400,400);
 }
 
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
+//draw sun
 function draw() {
 background(0,0,0)
-fill(255,0,0);
-ellipse(width/2,height/2,50,50);
+fill(sun.r,sun.g,sun.b);
+ellipse(sun.sunX,sun.sunY,sun.sunSize,sun.sunSize);
+
+//draw bird
+fill(bird.r,bird.g,bird.b);
+ellipse(bird.birdX,bird.birdY,bird.birdSize,bird.birdSize);
+
+bird.birdX=bird.birdX+1;
 }
